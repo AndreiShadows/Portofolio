40fbd706f1e715794abd55195ce6c5874107d731
Andrei Bunea <buneaandrei24@gmail.com>
2021-01-10 03:24:40 +0200

    Added a Popup modular component with animations

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import './App.css'
+import {useState} from "react";
+import {motion, AnimatePresence} from "framer-motion";
+//Components
+import SoonPopUp from "./components/SoonPopUp";
+
 
 
 function App() {
+  const [message, setMessage] = useState("");
+
   return (
     <>
+      <AnimatePresence>({message && (<SoonPopUp setMessage={setMessage}/>)}</AnimatePresence>
       <div className="container bg-white w-90 rounded-sm shadow-lg mx-auto mt-0 mt-md-5 mb-5 pb-3 px-4 pt-5">
         <h5 className="text-center mb-2">Who are we ?</h5>
-        <h2 className="mb-4 text-center">The only developer agency you will ever need</h2>
+        <h2 className="mb-4 text-center">The only development agency you will ever need</h2>
         <h5 className="mx-auto w-50 mb-5 font-weigt-light" style={{color: "#162945"}}>We are responsible for everything, from UI/UX Design, Development and Maintenance to event Databases and Back-End Servers. That means you just need to focus on the more important thing.</h5>
       
-        <div className="row mb-5 d-flex flex-md-row mx-0 justify-content-between">
+        <div className="row mb-3 d-flex flex-md-row mx-0 justify-content-between">
           <div className="col-md-3  mx-4 mb-4 value rounded-lg pl-4 pr-3 py-4" style={{background: "#F2F6F9"}}>
             <img src="https://img.icons8.com/ios-filled/50/000000/bios.png"  alt="development" className="mb-3"/>
             <h5 className="font-weight-bold mb-3">Development</h5>
@@ -27,6 +35,11 @@ function App() {
             <h5 className="font-weight-bold mb-3">24/7 maintenance</h5>
             <h6>We continuously make sure your website is running seamlessly. If not I assure you it will be solved within hours.</h6>
           </div>
+        </div>
+        <div className="w-100 d-flex align-items-center">
+          <button className="btn ml-4 mx-auto text-white font-weight-bold py-2 px-4" style={{background: "#EB1510"}} onClick={() => {
+            setMessage("HEya");
+          }}>Get started</button>
         </div>
       </div>
 
---
+++ src/components/SoonPopUp.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import "../App.css";
+import { motion } from "framer-motion";
+
+const SoonPopUp = ({setMessage}) => {
+    const parentVariant = {
+        visible: {
+            opacity: 1,
+            transition: {
+                when: "beforeChildren",
+                type: "tween"
+            }
+        },
+        hidden: {
+            opacity: 0,
+        },
+    }
+    const childrenVariant = {
+        visible: {
+            opacity: 1,
+            y: 0,
+            transition: {
+                type: "spring",
+                stiffness: 100
+            }
+        },
+        hidden: {
+            opacity: 0,
+            y: -200,
+        }
+    }
+
+    return (
+        <motion.div variants={parentVariant} initial="hidden" animate="visible" exit="hidden" style={{background: "rgba(0,0,0, 0.9)", width: "100%", height: "100%", position: 'fixed', top: 0, left: 0, zIndex: 2}} key="1">
+            <motion.div className="bg-white d-flex align-items-start justify-content-between rounded-lg w-50 mt-5 mx-auto p-4" variants={childrenVariant} key="2">
+                <div className="mr-3">
+                    <h6 className="font-weight-light text-dark mb-1 header mt-3">Just a test</h6>
+                    <h4 className="font-weight-bold text-dark mb-4">Something will change here, just wait and relax...</h4>
+                </div>
+                <div className="ml-auto"><img src="https://img.icons8.com/windows/32/000000/macos-close.png" onClick={() => {
+                    setMessage("");
+                }}/></div>
+            </motion.div>
+        </motion.div>
+    )
+}
+
+export default SoonPopUp;
