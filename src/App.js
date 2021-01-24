cd4dd93a3915b84fa9690f73a0c0b35e6fcae343
Andrei Bunea <buneaandrei24@gmail.com>
2021-01-25 01:02:00 +0200

    Changed the favico and the content.

--- src/App.js
+++ src/App.js
@@ -94,27 +94,27 @@ function App() {
         <>
           <AnimatePresence>({message && (<SoonPopUp setMessage={setMessage} message={message}/>)}</AnimatePresence>
           <div className="container bg-white w-90 rounded-sm shadow-lg mx-auto mt-0 mt-md-5 mb-5 pb-4 pb-md-5 px-4 pt-5">
-            <h5 className="text-center mb-2">Who are we ?</h5>
-            <h2 className="mb-4 text-center">The only development agency you will ever need</h2>
-            <h5 className="mx-auto w-50 mb-5 font-weigt-light" style={{color: "#162945"}}>We are responsible for everything, from UI/UX Design, Development and Maintenance to event Databases and Back-End Servers. That means you just need to focus on the more important thing.</h5>
+            <h5 className="text-center mb-2">Who am I?</h5>
+            <h2 className="mb-4 text-center">The only developer you'll ever need</h2>
+            <h5 className="mx-auto w-50 mb-5 font-weigt-light" style={{color: "#162945"}}>I am responsible for everything, from UI/UX Design, Development and Maintenance to event Databases and Back-End Servers. That means you just need to focus on the more important thing.</h5>
           
             <div className="row mb-3 d-flex flex-md-row mx-0 justify-content-between">
               <div className="col-md-3  mx-4 mb-4 value rounded-lg pl-4 pr-3 py-4" style={{background: "#F2F6F9"}}>
                 <img src="https://img.icons8.com/ios-filled/50/000000/bios.png"  alt="development" className="mb-3"/>
                 <h5 className="font-weight-bold mb-3">Development</h5>
-                <h6>A full-stack development, using the most efficient technologies for your project. </h6>
+                <h6>A full-stack development, using the most efficient technologies for your project.</h6>
               </div>
 
               <div className="col-md-3 mx-4 mb-4 value rounded-lg pl-4 pr-3 py-4" style={{background: "#F2F6F9"}}>
                 <img src="https://img.icons8.com/ios-filled/50/000000/database-restore.png" alt="servers" className="mb-3"/>
                 <h5 className="font-weight-bold mb-3">Databases and Servers</h5>
-                <h6>We make sure your client's data is safely stored and used, based on the most recent standards.</h6>
+                <h6>I make sure your client's data is safely stored and used, based on the most recent standards.</h6>
               </div>
 
               <div className=" col-md-3 mx-4 mb-4 value rounded-lg pl-4 pr-3 py-4" style={{background: "#F2F6F9"}}>
                 <img src="https://img.icons8.com/fluent-systems-filled/50/000000/thor-hammer.png" alt="maintenance" className="mb-3"/>
                 <h5 className="font-weight-bold mb-3">24/7 maintenance</h5>
-                <h6>We continuously make sure your website is running seamlessly. If not I assure you it will be solved within hours.</h6>
+                <h6>I continuously make sure your website is running seamlessly. If not I assure you it will be solved within hours.</h6>
               </div>
             </div>
             <div className="w-100 d-flex align-items-center">
@@ -128,7 +128,7 @@ function App() {
             <div className="col-12 col-md-6 mb-4 mt-4">
               <div className="text-white">
                 <h5 className="font-weight-bold mb-3">Documentation</h5>
-                <h6><a href="https://www.google.com/" className="text-white btn disabled p-0">About us</a></h6>
+                <h6><a href="https://www.google.com/" className="text-white btn disabled p-0">About Me</a></h6>
                 <h6><a href="https://www.google.com/" className="text-white btn disabled p-0">How it works?</a></h6>
                 <h6><a href="https://www.google.com/" className="text-white btn disabled p-0">Payments</a></h6>
                 <h6><a href="https://www.google.com/" className="text-white btn disabled p-0">Get Help</a></h6>
@@ -143,8 +143,8 @@ function App() {
               <AnimatePresence exitBeforeEnter>
                 {(email ? <motion.h6 key="errorLabel" initial={{y: -200, opacity: 0}} animate={{y: 0, opacity: 1}} exit={{y: -200, opacity: 0}} transition={{duration: 2}} className="text-white text-center text-md-left rounded-lg font-weight-bold mb-3 px-4 py-3" style={{backgroundColor: "#50c878"}}>We have succesfully sent you the verification code!</motion.h6> : "")}
               </AnimatePresence> 
-              <h6 className="font-weight-light text-white mb-1 header">Join us</h6>
-              <h4 className="font-weight-bold text-white mb-4">Be informed about every step we make</h4>
+              <h6 className="font-weight-light text-white mb-1 header">Join the community</h6>
+              <h4 className="font-weight-bold text-white mb-4">Be informed about every step I make</h4>
               <div className="d-flex align-items-center justify-content-between">
                   <input ref={emailRef} className="form-control p-2 mb-0 mr-4" name="email" id="email" placeholder="Email" style={{height: "100%"}} type="text" onChange={(e) => {setInputError(() => {if(sentEmail) {setSentEmail(false); setInputError("")}; setInputValue(e.target.value) })}}/>
                   <button
